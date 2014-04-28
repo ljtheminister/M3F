e2288bf20712d9d53432fc249e96c617682f0ce9
ljtheminister <john.c.min@gmail.com>
2014-04-28 11:14:33 -0400

    initial commit

---
+++ MixedMembershipMatrixFactorization.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cPickle as pickle
+
+import pylab
+import matplotlib.pyplot as plt
+
+class M3F:
+    def __init__(self, X):
+	self.X = X
+
+
+    def Gibbs_iteration(self):
